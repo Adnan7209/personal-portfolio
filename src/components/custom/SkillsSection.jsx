7da80c32b848a8bfd85d54d47adfaf260fa637cd
Adnan7209 <mdadnan28713@gmail.com>
2024-05-21 00:15:32 +0530

    css fix

--- src/components/custom/SkillsSection.jsx
+++ src/components/custom/SkillsSection.jsx
@@ -7,8 +7,8 @@ const SkillsSection = () => {
     <div className="relative  mt-20  ">
       <div className="absolute animate-pulse  blur-md top-0  bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 w-full h-full"></div>
 
-      <div className="flex md:flex-row flex-col mt-28 bg-neutral-900 rounded-3xl  relative">
-        <div className=" mt-16 flex flex-col gap-10">
+      <div className="flex md:flex-row flex-col mt-28 bg-neutral-900 rounded-3xl  relative  ">
+        <div className=" mt-16 flex flex-col gap-10 h-[76vh] overflow-auto">
           <div className="hover:scale-110 duration-300 transition-all flex flex-col justify-center items-center gap-2">
             <h2 className="text-4xl md:text-6xl text-white">Skills</h2>
             <div className="bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 rounded-3xl w-14 h-1"></div>
@@ -21,8 +21,8 @@ const SkillsSection = () => {
             upskilling and strive to keep up with technological advancements.
           </p>
         </div>
-        <div className="h-[80vh]  overflow-hidden mt-14 md:mt-0  ">
-          <div className="flex flex-wrap   animate-scroll-y-sm md:animate-scroll-y-md  lg:animate-scroll-y-lg">
+        <div className="h-[80vh]  overflow-hidden mt-14 md:mt-0 mb-2 md:mb-0 ">
+          <div className="flex flex-wrap  justify-center items-center animate-scroll-y-sm md:animate-scroll-y-md  lg:animate-scroll-y-lg">
             {techStackList.map((tech, index) => (
               <div key={index} className="hover:scale-110 duration-300 transition-all m-1 my-2 md:m-5">
                 <StackIcon
